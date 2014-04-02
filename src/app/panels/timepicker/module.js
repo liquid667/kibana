cd49cac176ba232d73ff9ff29e44391a7f0f33fc
Mikael Johansson <mikael@thematrix.se>
2014-04-02 13:57:33 +0200

    Updating time picker to 14 days instead for 2w

--- src/app/panels/timepicker/module.js
+++ src/app/panels/timepicker/module.js
@@ -36,7 +36,7 @@ function (angular, app, _, moment, kbn) {
     // Set and populate defaults
     var _d = {
       status        : "Stable",
-      time_options  : ['5m','15m','1h','6h','12h','24h','2d','7d','2w','30d'],
+      time_options  : ['5m','15m','1h','6h','12h','24h','2d','7d','14d','30d'],
       refresh_intervals : ['5s','10s','30s','1m','5m','15m','30m','1h','2h','1d'],
 
       timefield     : '@timestamp'
